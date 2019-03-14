2b082e5fc19facbb040f21d99220582e78926239
贰条 <531575302@qq.com>
2019-03-15 01:41:17 +0800

    mobile

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,22 @@
 import '../css/index.scss'
 
-console.log('index');
+const $D = $(document)
+const $W = $(window)
+
+$D.ready(function () {
+  var mySwiper = new Swiper ('.swiper-container', {
+    direction: 'horizontal', // 垂直切换选项
+    loop: true, // 循环模式选项
+
+    // 如果需要分页器
+    pagination: {
+      el: '.swiper-pagination',
+    },
+
+    // 如果需要前进后退按钮
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  })
+})
