ba7fbaa01c0b70ca6e0d50050dbc98f8467eb69d
贰条 <531575302@qq.com>
2019-03-17 00:59:20 +0800

    image swiper

--- src/js/index.js
+++ src/js/index.js
@@ -2,21 +2,39 @@ import '../css/index.scss'
 
 const $D = $(document)
 const $W = $(window)
-
+const ANIMATE_TIME = 8000
 $D.ready(function () {
-  var mySwiper = new Swiper ('.swiper-container', {
+  var imageSwiper = new Swiper ('#image-swiper', {
     direction: 'horizontal', // 垂直切换选项
     loop: true, // 循环模式选项
-
     // 如果需要分页器
     pagination: {
       el: '.swiper-pagination',
+      type: 'custom'
     },
-
+    autoplay: {
+      delay: ANIMATE_TIME
+    },
+    on:{
+      slideChangeTransitionEnd: function(){
+        let activeIndex = $('.swiper-slide-active').data('swiper-slide-index')
+        let $activePagination = $(`.swiper-pagination-item[data-index="${activeIndex}"]`)
+        console.log(activeIndex);
+        startProgressAnimate(activeIndex)
+      },
+    },
     // 如果需要前进后退按钮
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
+    // navigation: {
+    //   nextEl: '.swiper-button-next',
+    //   prevEl: '.swiper-button-prev',
+    // },
   })
 })
+
+const startProgressAnimate = (index) => {
+  let $pagination = $(`.swiper-pagination-item[data-index="${index}"]`)
+  let $progress = $pagination.find('.swiper-pagintaion-progress')
+
+  $('.swiper-pagintaion-progress').removeClass('progress-active')
+  $progress.addClass('progress-active')
+}
